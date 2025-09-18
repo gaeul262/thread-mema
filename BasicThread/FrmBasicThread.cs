9ec5e61cf9c502760ffd25e8568603f95b1fafc3
Michael Grace <mhgracek09@gmail.com>
2025-09-18 12:20:47 +0800

    Add files via upload

---
+++ BasicThread/FrmBasicThread.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BasicThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("-Before starting Thread -");
+            label1.Text = "-The thread is Running-";
+
+            Thread threadA = new Thread(new ThreadStart(StartThreadA));
+            Thread threadB = new Thread(new ThreadStart(StartThreadB));
+
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+
+            threadA.Start();
+            threadB.Start();
+        }
+
+        private void StartThreadA()
+        {
+            MyThreadClass.Thread1(listBox1);
+            SetEndText();
+        }
+
+        private void StartThreadB()
+        {
+            MyThreadClass.Thread1(listBox1);
+            listBox1.Items.Add("-End of thread-");
+        }
+
+        private void SetEndText()
+        {
+            if (label1.InvokeRequired)
+            {
+                label1.Invoke(new MethodInvoker(SetEndText));
+            }
+            else
+            {
+                label1.Text = "-End Of Thread-"; 
+            }
+        }
+    }
+}//Michael G.
---
+++ BasicThread/MyThreadClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BasicThread
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1(ListBox listBox)
+        {
+            for (int loopCount = 0; loopCount <= 5; loopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                string message;
+
+                if (!string.IsNullOrEmpty(thread.Name))
+                {
+                    message = "Name of Thread: Thread " + thread.Name + " Process = " + loopCount;
+                }
+                else
+                {
+                    message = "Thread ID: " + thread.ManagedThreadId + " = " + loopCount;
+                }
+
+                if (listBox.InvokeRequired)
+                {
+                    listBox.Invoke(new MethodInvoker(delegate
+                    {
+                        listBox.Items.Add(message);
+                    }));
+                }
+                else
+                {
+                    listBox.Items.Add(message);
+                }
+
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
